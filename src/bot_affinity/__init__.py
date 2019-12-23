049b88bc2e5fd69a487c12b7f633f75cbf030977
juanradiego <kilaweo@gmail.com>
2019-12-23 17:51:01 +0100

    Share method for film recomendation

--- src/bot_affinity/__init__.py
+++ src/bot_affinity/__init__.py
@@ -1,6 +1,7 @@
 import os
 from telegram.ext import Updater, CommandHandler
 import requests
+import logging
 
 _API_URL = 'https://filmaffinity-unofficial.herokuapp.com/api/'
 _API_SEARCH = 'search?'
@@ -11,57 +12,55 @@ def _api_film_id(query: str):
     return {'q': query, 'lang': 'EN'}
 
 
-def _help_find(bot):
-    return bot.message.reply_text(
-        "#TODO: HELP WITH FIND".format(bot.message.from_user.first_name))  # TODO
+def _api_film_metadata(film_id: int):
+    return requests.get(_API_URL + _API_MOVIE + str(film_id)).json()
 
 
-def _film_not_found(bot: str, query: str):
-    return bot.message.reply_text(
+def _help_find(bot_updater):
+    return bot_updater.message.reply_text(
+        "#TODO: HELP WITH FIND".format(bot_updater.message.from_user.first_name))  # TODO
+
+
+def _film_not_found(bot_updater, query: str):
+    return bot_updater.message.reply_text(
         'I was unable to find nothing with "{}". Sorry :_('.format(query))
-
-
-def _print_film_info(bot, film_request: dict, film_url: str):
-    description = '''
-**Title** : __{}__
-**Year** : __{}__
-**Duration** : __{}__
-**Director** : __{}__
-[Filmaffinty]({})
-    '''
-    description_md = description.format(film_request['title'], str(film_request['year']), str(film_request['duration']),
-                                        film_request['director'], film_url)
-    bot.message.reply_markdown(description_md)#, disable_notification=True)
 
 
 def _film_url(id: int):
     return 'https://www.filmaffinity.com/us/film{}.html'.format(str(id))
 
 
-def find(bot, context):
+def _find(bot_updater, context):
     if len(context.args) < 1:
-        return _help_find(bot)
+        return _help_find(bot_updater)
     query = str.join(' ', context.args)
     id_request = requests.get(_API_URL + _API_SEARCH, _api_film_id(query)).json()
     if len(id_request) < 1:
-        return _film_not_found(bot, query)
+        return _film_not_found(bot_updater, query)
     film_id = id_request[0]['id']
-    film_request = requests.get(_API_URL + _API_MOVIE + str(film_id)).json()
-    _print_film_info(bot, film_request, _film_url(film_id))
+    film_url = _film_url(film_id)
+    film_metadata = _api_film_metadata(film_id)
+    return {'id': film_id, 'url': film_url, 'metadata': film_metadata}
 
 
-def helpme(bot):
-    bot.message.reply_text(
+def share(bot_updater, context):
+    film = _find(bot_updater, context)
+    user = bot_updater.message.from_user
+    bot_updater.message.reply_text("{} recomends {} ".format(user.first_name, film['url']))
+
+
+def helpme(bot_updater, context):
+    bot_updater.message.reply_text(
         "Hello {}! I'm BotAffinity, I can find Filmaffinty pages for you. Try /find to begin.".format(
-            bot.message.from_user.first_name))
+            bot_updater.message.from_user.first_name))
 
 
-def bible_quote(bot):
-    bot.message.reply_text('https://www.youtube.com/watch?v=P-_A6gqda44')
+def bible_quote(bot_updater, context):
+    bot_updater.message.reply_text('https://www.youtube.com/watch?v=P-_A6gqda44')
 
 
-def pray(bot):
-    bot.message.reply_text(
+def pray(bot_updater, context):
+    bot_updater.message.reply_text(
         """
 This is my rifle. There are many like it, but this one is mine.
 My rifle is my best friend. It is my life. 
@@ -83,15 +82,19 @@ Amen.
 
 
 def main():
-    bot = Updater(os.environ['TOKEN'], use_context=True)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    bot_updater = Updater(os.environ['TOKEN'], use_context=True, workers=1)
 
-    bot.dispatcher.add_handler(CommandHandler('find', find))
-    bot.dispatcher.add_handler(CommandHandler('biblequote', bible_quote))
-    bot.dispatcher.add_handler(CommandHandler('pray', pray))
-    bot.dispatcher.add_handler(CommandHandler('help', helpme))
+    dispatcher = bot_updater.dispatcher
 
-    bot.start_polling()
-    bot.idle()
+    dispatcher.add_handler(CommandHandler('share', share))
+    dispatcher.add_handler(CommandHandler('biblequote', bible_quote))
+    dispatcher.add_handler(CommandHandler('pray', pray))
+    dispatcher.add_handler(CommandHandler('help', helpme))
+
+    bot_updater.start_polling()
+    bot_updater.idle()
 
 
 if __name__ == '__main__':
